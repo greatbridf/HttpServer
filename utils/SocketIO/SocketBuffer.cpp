722759d56533b319af8ea3ebf80b33af1c1c9ef7
greatbridf <guobingfang20040616@gmail.com>
2020-02-29 14:30:12 +0800

    style: reformat SocketBuffer.cpp

--- utils/SocketIO/SocketBuffer.cpp
+++ utils/SocketIO/SocketBuffer.cpp
@@ -10,74 +10,74 @@ namespace greatbridf
     SocketBuffer::SocketBuffer(Socket& socket)
         : socket(socket)
     {
-      this->readBuf = new char[BUF_SIZE];
-      this->writeBuf = new char[BUF_SIZE];
+        this->readBuf = new char[BUF_SIZE];
+        this->writeBuf = new char[BUF_SIZE];
 
-      setg(this->readBuf, this->readBuf, this->readBuf);
-      setp(this->writeBuf, this->writeBuf + BUF_SIZE);
+        setg(this->readBuf, this->readBuf, this->readBuf);
+        setp(this->writeBuf, this->writeBuf + BUF_SIZE);
     }
 
     SocketBuffer::~SocketBuffer()
     {
-      delete[] this->readBuf;
-      delete[] this->writeBuf;
+        delete[] this->readBuf;
+        delete[] this->writeBuf;
     }
 
     int SocketBuffer::underflow()
     {
-      size_t n = ::recv(socket.socket, eback(), BUF_SIZE, 0);
+        size_t n = ::recv(socket.socket, eback(), BUF_SIZE, 0);
 
-      if (n <= 0)
-      {
-        return EOF;
-      }
-      else
-      {
-        setg(eback(), eback(), eback() + n);
-      }
+        if (n <= 0)
+        {
+            return EOF;
+        }
+        else
+        {
+            setg(eback(), eback(), eback() + n);
+        }
 
-      return *eback();
+        return *eback();
     }
 
     int SocketBuffer::sync()
     {
-      if (socket.closed) return EOF;
+        if (socket.closed) return EOF;
 
-      size_t sent = 0;
-      size_t total = pptr() - pbase();
+        size_t sent = 0;
+        size_t total = pptr() - pbase();
 
-      while (sent < total)
-      {
-        size_t n = ::send(socket.socket, writeBuf, total - sent, 0);
-
-        if (n > 0)
+        while (sent < total)
         {
-          sent += n;
-        }
-        else
-        {
-          return EOF;
-        }
-      }
+            size_t n = ::send(socket.socket, writeBuf, total - sent, 0);
 
-      setp(pbase(), pbase() + BUF_SIZE);
-      pbump(0);
+            if (n > 0)
+            {
+                sent += n;
+            }
+            else
+            {
+                return EOF;
+            }
+        }
 
-      return 0;
+        setp(pbase(), pbase() + BUF_SIZE);
+        pbump(0);
+
+        return 0;
     }
 
     int SocketBuffer::overflow(int c)
     {
-      if (sync() == EOF)
-      {
-        return EOF;
-      }
+        if (sync() == EOF)
+        {
+            return EOF;
+        }
 
-      if (c != EOF)
-      {
-        sputc(c);
-      }
-      return c;
+        if (c != EOF)
+        {
+            sputc(c);
+        }
+        return c;
     }
 
 }
