a47ec3cc012fdc0ba869af29fa4b843df2477ed6
greatbridf <guobingfang20040616@gmail.com>
2020-07-16 16:44:55 +0000

    fix: remove extra slash in plugin dir variable

--- src/marco_definitions.hpp
+++ src/marco_definitions.hpp
@@ -8,7 +8,7 @@
 #ifndef __PREFIX
 #define __PREFIX "/usr/local"
 #endif
-#define _GREATBRIDF_PLUGIN_DIRECTORY __PREFIX "/lib/HttpParser/plugins/"
+#define _GREATBRIDF_PLUGIN_DIRECTORY __PREFIX "/lib/HttpParser/plugins"
 
 #ifndef _GREATBRIDF_CONFIG_PATH
 #define _GREATBRIDF_CONFIG_PATH __PREFIX "/etc/HttpParser.conf.json"
